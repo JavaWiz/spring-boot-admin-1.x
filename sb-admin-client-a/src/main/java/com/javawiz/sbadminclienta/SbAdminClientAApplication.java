2a4faf33cfacd7e6b13d84256367fc2959de5eb4
Ranjan Sai <JavaWiz@users.noreply.github.com>
2018-05-14 01:59:20 +0530

    spring boot admin example using codecentric

---
+++ sb-admin-client-a/src/main/java/com/javawiz/sbadminclienta/SbAdminClientAApplication.java
@@ -0,0 +1,12 @@
+package com.javawiz.sbadminclienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdminClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAdminClientAApplication.class, args);
+	}
+}
---
+++ sb-admin-client-b/src/main/java/com/javawiz/sbadminclientb/SbAdminClientBApplication.java
@@ -0,0 +1,12 @@
+package com.javawiz.sbadminclientb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbAdminClientBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbAdminClientBApplication.class, args);
+	}
+}
